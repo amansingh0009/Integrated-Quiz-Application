be6446d0a08af9f61a595a769d22b7a5cada1971
Aman <amansinghcc07@gmail.com>
2024-05-14 23:19:12 +0530

    DOne

---
+++ timer.js
@@ -0,0 +1,9 @@
+let count = 60;
+const timer = setInterval(function() {
+  count--;
+  console.log(count);
+  if (count === 0) {
+    clearInterval(timer);
+    console.log("Time's up!");
+  }
+}, 1000);
