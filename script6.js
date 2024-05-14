3010ea455df2496606b4ca470faff8e561896a82
Aman <amansinghcc07@gmail.com>
2024-05-14 23:12:41 +0530

    Javascript added

---
+++ script6.js
@@ -0,0 +1,451 @@
+const startBtn = document.querySelector('.start-btn');
+
+const popupInfo = document.querySelector('.popup-info');
+
+const exitBtn = document.querySelector('.exit-btn');
+
+const main = document.querySelector('.main');
+
+const continueBtn = document.querySelector('.continue-btn');
+
+const quizSection = document.querySelector('.quiz-section');
+
+const quizBox = document.querySelector('.quiz-box');
+
+const time_line = document.querySelector(".quiz-header .time_line");
+
+const timeText = document.querySelector(".timer .time_left_txt");
+
+const timeCount = document.querySelector(".timer .timer_sec");
+
+const resultBox = document.querySelector('.result-box');
+
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+
+const goHomeBtn = document.querySelector('.goHome-btn');
+
+ 
+
+ 
+
+startBtn.onclick = () => {
+
+    popupInfo.classList.add('active');
+
+    main.classList.add('active');
+
+}
+
+ 
+
+exitBtn.onclick = () => {
+
+    popupInfo.classList.remove('active');
+
+    main.classList.remove('active');
+
+}
+
+ 
+
+continueBtn.onclick = () => {
+
+    quizSection.classList.add('active');
+
+    popupInfo.classList.remove('active');
+
+    main.classList.remove('active');
+
+    quizBox.classList.add('active');
+
+   
+
+    showQuestions(0);
+
+    questionCounter(1);
+
+    headerScore();
+
+    startTimer(10); //calling startTimer function
+
+    startTimerLine(0);
+
+}
+
+ 
+
+tryAgainBtn.onclick = () => {
+
+    quizBox.classList.add('active');
+
+    nextBtn.classList.remove('active');
+
+    resultBox.classList.remove('active');
+
+ 
+
+    timeValue = 10;
+
+    questionCount = 0;
+
+    questionNumb = 1;
+
+    userScore = 0;
+
+    widthValue = 0;
+
+    showQuestions(questionCount);
+
+    questionCounter(questionNumb);
+
+    clearInterval(counter);
+
+    clearInterval(counterLine);
+
+    startTimer(timeValue);
+
+    startTimerLine(widthValue);
+
+    timeText.textContent = "Time Left";
+
+ 
+
+    headerScore();
+
+}
+
+ 
+
+goHomeBtn.onclick = () => {
+
+    quizSection.classList.remove('active');
+
+    nextBtn.classList.remove('active');
+
+   resultBox.classList.remove('active');
+
+ 
+
+    questionCount = 0;
+
+    questionNumb = 1;
+
+    userScore = 0;
+
+    showQuestions(questionCount);
+
+    questionCounter(questionNumb);
+
+}
+
+ 
+
+let timeValue = 10;
+
+let questionCount = 0;
+
+let questionNumb = 1;
+
+let userScore = 0;
+
+let counter;
+
+let counterLine;
+
+let widthValue = 0;
+
+ 
+
+const nextBtn = document.querySelector('.next-btn');
+
+ 
+
+nextBtn.onclick = () => {
+
+    if (questionCount < questions.length - 1) {
+
+        questionCount++;
+
+        showQuestions(questionCount);
+
+        questionNumb++;
+
+        questionCounter(questionNumb);
+
+        clearInterval(counter);
+
+        clearInterval(counterLine);
+
+        startTimer(timeValue);
+
+        startTimerLine(widthValue);
+
+        timeText.textContent = "Time Left";
+
+        nextBtn.classList.add('active');
+
+    }
+
+    else {
+
+        clearInterval(counter);
+
+        clearInterval(counterLine);
+
+        showResultBox();
+
+    }
+
+ 
+
+}
+
+ 
+
+const optionList = document.querySelector('.option-list');
+
+ 
+
+// getting questions and options from array
+
+function showQuestions(index) {
+
+    const questionText = document.querySelector('.question-text');
+
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+ 
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+
+        <div class="option"><span>${questions[index].options[1]}</span></div>
+
+        <div class="option"><span>${questions[index].options[2]}</span></div>
+
+        <div class="option"><span>${questions[index].options[3]}</span></div>`
+
+       
+
+    optionList.innerHTML = optionTag;
+
+ 
+
+    const option = document.querySelectorAll('.option');
+
+    for (let i = 0; i < option.length; i++) {
+
+        option[i].setAttribute('onclick',  'optionSelected(this)');
+
+    }
+
+}
+
+ 
+
+function optionSelected(answer) {
+
+    clearInterval(counter);
+
+    clearInterval(counterLine);
+
+    let userAnswer = answer.textContent;
+
+    let correctAnswer = questions[questionCount].answer;
+
+    let allOptions =optionList.children.length;
+
+   
+
+    if (userAnswer == correctAnswer) {
+
+        answer.classList.add('correct');
+
+        userScore += 1;
+
+        headerScore();
+
+    }
+
+    else{
+
+        answer.classList.add('incorrect');
+
+       
+
+        // if answer incorrect, auto selected correct answer
+
+        for (let i = 0; i < allOptions; i++) {
+
+            if (optionList.children[i].textContent == correctAnswer) {
+
+                optionList.children[i].setAttribute('class', 'option correct');
+
+            }
+
+        }
+
+    }
+
+ 
+
+    // if user has selected, disabled all options
+
+    for (let i = 0; i < allOptions; i++) {
+
+        optionList.children[i].classList.add('disabled');
+
+    }
+
+ 
+
+    nextBtn.classList.add('active');
+
+}
+
+ 
+
+function questionCounter(index) {
+
+    const questionTotal = document.querySelector('.question-total');
+
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+
+}
+
+ 
+
+// function headerScore() {
+
+    // const headerScoreText = document.querySelector('.header-score');
+
+    // headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
+
+// }
+
+ 
+
+function showResultBox() {
+
+    quizBox.classList.remove('active');
+
+    resultBox.classList.add('active');
+
+ 
+
+    const scoreText = document.querySelector('.score-text');
+
+    scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+   
+
+    const circularProgress = document.querySelector('.circular-progress');
+
+    const progressvalue = document.querySelector('.progress-value');
+
+    let progressStartValue = -1;
+
+    let progressEndValue = (userScore / questions.length) * 100;
+
+    let speed =20;
+
+ 
+
+    let progress = setInterval(() => {
+
+        progressStartValue++;
+
+ 
+
+        progressvalue.textContent = `${progressStartValue}%`;
+
+        circularProgress.style.background = `conic-gradient(red ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+
+ 
+
+        if (progressStartValue == progressEndValue) {
+
+            clearInterval(progress);
+
+        }
+
+    }, speed);
+
+}
+
+ 
+
+function startTimer(time){
+
+    counter = setInterval(timer, 1000);
+
+    function timer(){
+
+        timeCount.textContent = time; //changing the value of timeCount with time value
+
+        time--; //decrement the time value
+
+        if(time < 9){ //if timer is less than 9
+
+            let addZero = timeCount.textContent;
+
+            timeCount.textContent = "0" + addZero; //add a 0 before time value
+
+        }
+
+        if(time < 0){ //if timer is less than 0
+
+            clearInterval(counter); //clear counter
+
+            timeText.textContent = "Time Off"; //change the time text to time off
+
+            const allOptions = optionList.children.length; //getting all option items
+
+            let correcAns = questions[questionCount].answer; //getting correct answer from array
+
+            for(i=0; i < allOptions; i++){
+
+                if(optionList.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
+
+                    optionList.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+
+                    optionList.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+
+                    console.log("Time Off: Auto selected correct answer.");
+
+                }
+
+            }
+
+            for(i=0; i < allOptions; i++){
+
+                optionList.children[i].classList.add("disabled"); //once user select an option then disabled all options
+
+            }
+
+            nextBtn.classList.add("show"); //show the next button if user selected any option
+
+        }
+
+    }
+
+}
+
+function startTimerLine(time){
+
+    counterLine = setInterval(timer, 29);
+
+    function timer(){
+
+        time += 1; //upgrading time value with 1
+
+        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+
+        if(time > 549){ //if time value is greater than 549
+
+            clearInterval(counterLine); //clear counterLine
+
+        }
+
+    }
+
+}
