051dd58d188bd02e81356b096fd6df8728e4e600
SnehaKapoor2004 <157450175+SnehaKapoor2004@users.noreply.github.com>
2024-05-15 12:16:35 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	char op;
+	
+	switch(op)
+	{
+		case '+':
+			printf("valid", a+b);
+			break;
+	 case '-':
+	 printf("valid ", a-b);
+	 break;	
+	}
+	
+}
